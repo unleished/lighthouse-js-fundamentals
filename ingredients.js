f9f32c95899f554e4dd384e7f95fcdbf6631576d
Leisha Magsisi <leisha.gallagher@gmail.com>
2018-07-10 12:18:26 +0000

    Adding Ingredient Loop Exercise

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+console.log("Ingredients:");
+var i = 0
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+console.log("Ingredients:");
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+console.log("Ingredients:");
+for (var i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
